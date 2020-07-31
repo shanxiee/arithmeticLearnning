994f6e734cfc85a40d76b3e83a237b09d781e9cc
xiee <shanxiee@qq.com>
2020-07-31 15:10:52 +0800

    Scenes界面绘制小工具

---
+++ Assets/Scripts/Tools/DrawScenesGuiTools.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector;
+using UnityEditor.SceneManagement;
+using System;
+
+public class DrawScenesGuiTools : Editor {
+
+    [InitializeOnLoadMethod]
+    private static  void PackageLuaAndStartScenes()
+    {
+
+        SceneView.onSceneGUIDelegate += delegate(SceneView sceneView)
+        {
+            if (!Application.isPlaying)
+            {
+                Handles.BeginGUI();
+
+                if (GUI.Button(new Rect(Screen.width - 150f, 50f, 100, 20f), "打包Lua后启动"))
+                {
+                    Action playScenes = delegate
+                    {
+                        EditorApplication.isPlaying = true;
+                    };  
+                }
+                Handles.EndGUI();
+            }
+
+        };
+    }
+
+}
